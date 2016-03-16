c0bb0183e0e99f14e92ac9d242149b77ee8955dd
victoria_felice <wwwvictoria@gmail.com>
2016-03-15 22:15:01 -0400

    add active class to navigation and modify Portfolio page

--- PortfolioASPdotNET.Tests/UnitTest1.cs
+++ PortfolioASPdotNET.Tests/UnitTest1.cs
@@ -12,11 +12,5 @@ namespace PortfolioASPdotNET.Tests {
 			var result = homeController.Portfolio() as ViewResult;
 			Assert.AreEqual("", result.ViewName);
 		}
-		[TestMethod]
-		public void ContactFormSaysThanks() {
-			var homeController = new HomeController();
-			var result = homeController.Contact("I love your portfolio") as ViewResult;
-			Assert.IsNotNull(result.ViewBag.TheMessage);
-		}
 	}
 }
